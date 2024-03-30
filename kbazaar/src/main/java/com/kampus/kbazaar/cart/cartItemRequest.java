1f46c215bcce67819d2e31f205826c37fc281884
Bento39 <bentee@hotmail.co.th>
2024-03-31 02:20:16 +0700

    add get set CartItem and CartItemRequest

---
+++ kbazaar/src/main/java/com/kampus/kbazaar/cart/cartItemRequest.java
@@ -0,0 +1,34 @@
+package com.kampus.kbazaar.cart;
+
+public class CartItemRequest {
+    private String code;
+    private String productSkus;
+
+    public CartItemRequest(String code, String productSkus) {
+        this.code = code;
+        this.productSkus = productSkus;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getProductSkus() {
+        return productSkus;
+    }
+
+    public void setProductSkus(String productSkus) {
+        this.productSkus = productSkus;
+    }
+
+    public String toString() {
+        return "{" +
+                "\"code\":\"" + code + "\"," +
+                "\"productSkus\":\"" + productSkus + "\"" +
+                "}";
+    }
+}
