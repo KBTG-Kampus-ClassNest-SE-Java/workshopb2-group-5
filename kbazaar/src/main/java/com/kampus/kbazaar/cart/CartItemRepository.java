ffed00f2291b4354ee1e03c64b39cf778fec589d
Jonathan Chen <sabza178@gmail.com>
2024-03-30 22:03:36 +0700

    add findAllByUsername in CarItemRepository

--- kbazaar/src/main/java/com/kampus/kbazaar/cart/CartItemRepository.java
+++ kbazaar/src/main/java/com/kampus/kbazaar/cart/CartItemRepository.java
@@ -1,7 +1,10 @@
 package com.kampus.kbazaar.cart;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {}
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    List<CartItem> findAllByUsername(String username);
+}
