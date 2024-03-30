71022bc5c889c414cc75a14e686363149d967c12
ArtCode101 <sanayut12.service@gmail.com>
2024-03-30 17:05:38 +0700

    add @repository for cart

--- kbazaar/src/main/java/com/kampus/kbazaar/cart/CartRepository.java
+++ kbazaar/src/main/java/com/kampus/kbazaar/cart/CartRepository.java
@@ -1,5 +1,7 @@
 package com.kampus.kbazaar.cart;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {}
