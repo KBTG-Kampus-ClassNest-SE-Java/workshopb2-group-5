671f30138f153c2d29a7a3f3b15e4a7c796089d5
ArtCode101 <sanayut12.service@gmail.com>
2024-03-30 17:03:38 +0700

    add service for cart

--- kbazaar/src/main/java/com/kampus/kbazaar/cart/CartService.java
+++ kbazaar/src/main/java/com/kampus/kbazaar/cart/CartService.java
@@ -3,4 +3,14 @@ package com.kampus.kbazaar.cart;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CartService {}
+public class CartService {
+
+    private CartRepository cartRepository;
+
+    private CartItemRepository cartItemRepository;
+
+    public CartService(CartRepository cartRepository, CartItemRepository cartItemRepository) {
+        this.cartRepository = cartRepository;
+        this.cartItemRepository = cartItemRepository;
+    }
+}
