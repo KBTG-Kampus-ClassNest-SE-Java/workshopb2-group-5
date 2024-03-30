47511faeff5ed67caadab2882f0a5bd36fdc091c
Bento39 <bentee@hotmail.co.th>
2024-03-31 03:43:15 +0700

    update Cart item service

---
+++ kbazaar/src/main/java/com/kampus/kbazaar/cart/CartItemService.java
@@ -0,0 +1,36 @@
+package com.kampus.kbazaar.cart;
+
+import com.kampus.kbazaar.promotion.PromotionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CartItemService {
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+    private final PromotionRepository promotionRepository;
+
+    public List<CartItem> getCartItemsByUsernameAndSku(String username, String sku) {
+        return cartItemRepository.findAllByUsernameAndSku(username, sku);
+    }
+
+    public BigDecimal calculateTotalDiscount(List<CartItem> cartItems) {
+        return cartItems.stream()
+                .map(CartItem::getDiscount)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public BigDecimal calculateSubTotal(List<CartItem> cartItems) {
+        return cartItems.stream()
+                .map(CartItem::getPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public BigDecimal calculateGrandTotal(BigDecimal discount, BigDecimal subtTotal){
+        return subtTotal.subtract(discount);
+    }
+}
